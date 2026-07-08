1cb107f5bbb59ea0d84acef7319a8d44006231f5
Navneet Chauhan <navneetrajpushp@gmail.com>
2026-07-09 00:07:08 +0530

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+            
+        }
+        [HttpGet]
+        [Route("ProductsList")]
+        public IActionResult GetProductsList()
+         {
+        var products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Laptop",
+                Price = 50000
+            },
+
+            new Product
+            {
+                Id = 2,
+                Name = "Mobile",
+                Price = 30000
+            },
+
+            new Product
+            {
+                Id = 3,
+                Name = "Keyboard",
+                Price = 2000
+            }
+        };
+
+        return Ok(products);
+        //return new OkObjectResult(products); roslyn doest no ok it converts into class OkObjectResult
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure middleware
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+// Required for integration testing
